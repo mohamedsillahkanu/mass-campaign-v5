f965802203902cfc7e277139a92aeecd5e9b11af
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2026-02-08 09:40:34 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'itn-campaign-v9';
+const CACHE_NAME = 'itn-campaign-v11';
 const ASSETS = [
     './',
     'index.html',
@@ -10,10 +10,7 @@ const ASSETS = [
     'https://github.com/mohamedsillahkanu/gdp-dashboard-2/raw/6c7463b0d5c3be150aafae695a4bcbbd8aeb1499/ICF-SL.jpg',
     'https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap',
     'https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js',
-    'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-    'https://cdn.jsdelivr.net/npm/qrcode-generator@1.4.4/qrcode.min.js'
+    'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'
 ];
 
 self.addEventListener('install', e => {
